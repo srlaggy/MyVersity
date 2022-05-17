c6253d5afd9d72205ec74ec18334fe8b8f46b2c2
srlaggy <ignacio.ulloa@sansano.usm.cl>
2022-05-17 12:33:01 -0400

    primera vista no funcional

--- app/src/main/java/com/example/myversity/SettingsActivity.java
+++ app/src/main/java/com/example/myversity/SettingsActivity.java
@@ -1,16 +1,14 @@
 package com.example.myversity;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_settings);
     }
 }
