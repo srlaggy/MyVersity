5968c50cc5236ad7aa6586d16963906abe5351af
srlaggy <ignacio.ulloa@sansano.usm.cl>
2022-07-04 03:54:47 -0400

    Adaptador para ponderaciones
    Se agrego adaptador para ponderaciones, falta definir el comportamiento cuando se clickea siguiente

--- app/src/main/java/com/example/myversity/DialogFragmentAgregarEval2.java
+++ app/src/main/java/com/example/myversity/DialogFragmentAgregarEval2.java
@@ -11,21 +11,32 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.example.myversity.adapters.RowExportar;
+import com.example.myversity.adapters.RowExportarAdapter;
+import com.example.myversity.adapters.RowNotasEvaluacion;
+import com.example.myversity.adapters.RowNotasEvaluacionAdapter;
 import com.example.myversity.db.DbAsignaturas;
 import com.example.myversity.db.DbTipoPromedio;
+import com.example.myversity.entidades.Asignaturas;
+import com.example.myversity.entidades.Notas;
 import com.example.myversity.entidades.TipoPromedio;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class DialogFragmentAgregarEval2 extends androidx.fragment.app.DialogFragment {
     private Button btn_dialogFragment_agre_eval2_cancelar, btn_dialogFragment_agre_eval2_confirmar;
-
+    private List<RowNotasEvaluacion> rows = new ArrayList<>();
+    private ListView listViewPonderaciones;
+    private Integer nroNotas = Integer.parseInt(VistaAsignaturaFragment.cant_eval_agregar);
 
     @Nullable
     @Override
@@ -34,6 +45,14 @@ public class DialogFragmentAgregarEval2 extends androidx.fragment.app.DialogFrag
         super.onCreateView(inflater, container, savedInstanceState);
         btn_dialogFragment_agre_eval2_cancelar = view.findViewById(R.id.dialogFragment_agre_eval2_cancelar);
         btn_dialogFragment_agre_eval2_confirmar = view.findViewById(R.id.dialogFragment_agre_eval2_confirmar);
+        listViewPonderaciones = (ListView) view.findViewById(R.id.lista_ponderaciones_notas_evaluacion);
+        if(nroNotas > 0){
+            setearRows();
+            listViewPonderaciones.setAdapter(new RowNotasEvaluacionAdapter(getActivity().getApplicationContext(), rows));
+        }
+        if(rows.isEmpty()){
+            Toast.makeText(getActivity().getApplicationContext(), "No existen notas", Toast.LENGTH_LONG).show();
+        }
 
         // BOTON CANCELAR
         btn_dialogFragment_agre_eval2_cancelar.setOnClickListener(new View.OnClickListener() {
@@ -47,7 +66,38 @@ public class DialogFragmentAgregarEval2 extends androidx.fragment.app.DialogFrag
         btn_dialogFragment_agre_eval2_confirmar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                String aux = "|" + rows.get(0).getValor() + "|" + rows.get(1).getValor() + "|" + rows.get(2).getValor();
+                Log.d("ejemplo_ponderacion", aux);
+                // estados
+                // 0 -> esta bien
+                // 1 -> campos vacios
+                // 2 -> error de formato
+                // 3 -> no suman 100
+                int estado = revisarRows();
+                switch (estado){
+                    case 0:
+                        // hacer el redirect hacia la siguiente pesta;a
+                        // to do esta bien
+                        // deberias guardar la `Notas` que esta dentro de `rows`
+                        //      Esta contiene una Nota vacia donde el unico valor cambiado es el valor del `Peso`
+                        for(RowNotasEvaluacion r : rows){
+                            Log.d("ejemplo_ponderacion", r.getNota().getPeso());
+                        }
+                        Toast.makeText(getActivity().getApplicationContext(), "LISTO", Toast.LENGTH_LONG).show();
+                        break;
+                    case 1:
+                        Toast.makeText(getActivity().getApplicationContext(), "Campos vacios", Toast.LENGTH_LONG).show();
+                        break;
+                    case 2:
+                        Toast.makeText(getActivity().getApplicationContext(), "Error en el formato", Toast.LENGTH_LONG).show();
+                        break;
+                    case 3:
+                        Toast.makeText(getActivity().getApplicationContext(), "Las ponderaciones no suman 100%", Toast.LENGTH_LONG).show();
+                        break;
+                    default:
+                        Toast.makeText(getActivity().getApplicationContext(), "Error", Toast.LENGTH_LONG).show();
+                        break;
+                }
             }
         });
 
@@ -55,4 +105,39 @@ public class DialogFragmentAgregarEval2 extends androidx.fragment.app.DialogFrag
         return view;
     }
 
+    private void setearRows(){
+        RowNotasEvaluacion row = null;
+        Notas n = null;
+        for(int i=0; i<nroNotas; i++){
+            row = new RowNotasEvaluacion();
+            row.setTitulo("Nota " + (i+1) + " :");
+            row.setValor("");
+            row.setNota(new Notas());
+            rows.add(row);
+        }
+    }
+
+    private int revisarRows(){
+        // estados
+        // 0 -> esta bien
+        // 1 -> campos vacios
+        // 2 -> error de formato
+        // 3 -> no suman 100
+        int num, cont = 0;
+        for(RowNotasEvaluacion r : rows) {
+            if (Objects.equals(r.getValor(), "")) {
+                return 1;
+            }
+            try{
+                num = Integer.parseInt(r.getValor());
+                cont += num;
+            } catch (NumberFormatException e) {
+                return 2;
+            }
+        }
+        if(cont != 100){
+            return 3;
+        }
+        return 0;
+    }
 }
---
+++ app/src/main/java/com/example/myversity/adapters/RowNotasEvaluacionAdapter.java
@@ -0,0 +1,78 @@
+package com.example.myversity.adapters;
+
+import android.content.Context;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.myversity.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RowNotasEvaluacionAdapter extends ArrayAdapter<RowNotasEvaluacion> {
+    private Context mContext;
+    private List<RowNotasEvaluacion> rows = new ArrayList<>();
+    private boolean isOnValueChanged = false;
+
+    public RowNotasEvaluacionAdapter(@NonNull Context context, List<RowNotasEvaluacion> objects) {
+        super(context, 0, objects);
+        mContext = context;
+        rows = objects;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        Integer pos = position;
+        View listItem = convertView;
+        if(listItem == null){
+            listItem = LayoutInflater.from(mContext).inflate(R.layout.list_ponderaciones_notas_evaluacion,parent,false);
+        }
+
+        RowNotasEvaluacion currentRow = rows.get(position);
+
+        TextView text = (TextView) listItem.findViewById(R.id.texto_ponderacion_item);
+        text.setText(currentRow.getTitulo());
+
+        EditText input = (EditText) listItem.findViewById(R.id.input_ponderacion_item);
+        input.setText(currentRow.getValor());
+
+        input.addTextChangedListener(new TextWatcher() {
+                 @Override
+                 public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                 }
+
+                 @Override
+                 public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                     isOnValueChanged = true;
+                 }
+
+                 @Override
+                 public void afterTextChanged(Editable editable) {
+                     if (isOnValueChanged) {
+                         isOnValueChanged = false;
+
+                         Log.d("ejemplo_ponderacion", pos.toString());
+                         rows.get(position).setValor(editable.toString());
+                         rows.get(position).getNota().setPeso(editable.toString());
+                     }
+                 }
+             }
+        );
+
+        return listItem;
+    }
+}
