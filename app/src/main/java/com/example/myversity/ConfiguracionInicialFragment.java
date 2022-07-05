66a47d27a5d88398d8f2a9c8835d46e98c0f154e
srlaggy <ignacio.ulloa@sansano.usm.cl>
2022-07-05 18:59:18 -0400

    fix a config inicial desde editar

--- app/src/main/java/com/example/myversity/ConfiguracionInicialFragment.java
+++ app/src/main/java/com/example/myversity/ConfiguracionInicialFragment.java
@@ -39,6 +39,8 @@ public class ConfiguracionInicialFragment extends Fragment {
     // 1 -> viene desde las asignaturas
     // 2 -> editar la configuración de una asignatura
     private Integer estado = 0;
+    private Asignaturas asig_est2;
+    private ConfiguracionInicial configPrimera;
 
     public ConfiguracionInicialFragment() {
         // Required empty public constructor
@@ -79,7 +81,12 @@ public class ConfiguracionInicialFragment extends Fragment {
         orientacion = (RadioGroup) view.findViewById(R.id.radio_orientacion_config_inicial);
         tipo = (RadioGroup) view.findViewById(R.id.radio_tiponota_config_inicial);
 
-        ConfiguracionInicial configPrimera = dbConfigInicial.buscarPrimeraConfiguracion();
+        if(estado == 2){
+            asig_est2 = AsignaturasFragment.getAsignatura_seleccionada();
+            configPrimera = dbConfigInicial.buscarRegistro(asig_est2.getId_configInicial());
+        } else {
+            configPrimera = dbConfigInicial.buscarPrimeraConfiguracion();
+        }
         dbConfigInicial.close();
         setDefaultValues(configPrimera);
 
@@ -174,8 +181,6 @@ public class ConfiguracionInicialFragment extends Fragment {
                             ((MainActivity) activity).binding.bottombar.setSelectedItemId(R.id.btn_nav_asignatura);
                         }
                     } else if (estado == 2){
-                        Asignaturas asig_est2 = AsignaturasFragment.getAsignatura_seleccionada();
-
                         // SE AGREGA A LA BD
                         DbAsignaturas dbAsignaturas = new DbAsignaturas(getActivity().getApplicationContext());
                         Long idAux = dbAsignaturas.actualizarConfigInicial(asig_est2.getId(), asig_est2.getId_configInicial(), configAux);
